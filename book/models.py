0014a3bf5c42b88e5b13db80d7c9cdf1fd72372d
soulnazz <kutmanbekkyzydilnaz@gmail.com>
2023-10-14 09:28:30 +0600

    Initial commit

---
+++ book/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    image = models.ImageField(upload_to='')
+    cost = models.SmallIntegerField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
