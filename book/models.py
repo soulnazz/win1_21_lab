83a328f6ba38b27ebb89f937b63e716731b80aea
soulnazz <kutmanbekkyzydilnaz@gmail.com>
2023-10-16 01:30:51 +0600

    lab2

--- book/models.py
+++ book/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
 class Book(models.Model):
+    ACTUALITY = (
+        ('Актуален', 'Актуален'),
+        ('50 на 50', '50 на 50'),
+    )
+
 
     title = models.CharField(max_length=30)
     description = models.TextField()
     image = models.ImageField(upload_to='')
+    actuality = models.CharField(max_length=100, choices=ACTUALITY, default=ACTUALITY[0], null=True)
+    video = models.URLField(null=True)
     cost = models.SmallIntegerField()
     created_date = models.DateTimeField(auto_now_add=True)
+
+
 
 
     def __str__(self):
--- book/urls.py
+++ book/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.book_view),
+    path('', views.book_view),
+    path('book_detail/<int:id>/', views.book_detail_view),
 ]
